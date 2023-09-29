7e76feb02963538eabd0d9e613aeed24b653f24b
Byeongjun Moon <32949241+bjmoonn@users.noreply.github.com>
2023-09-28 17:18:43 -0700

    content

---
+++ components/NavBar.tsx
@@ -0,0 +1,11 @@
+import styles from "./NavBar.module.css";
+
+export default function NavBar() {
+    return (
+        <div className={styles.nav}>
+            <div className={styles.title}>
+                ACAD 276
+            </div>
+        </div>
+    );
+}
